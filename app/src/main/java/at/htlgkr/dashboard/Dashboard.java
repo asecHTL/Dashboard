e239749d1824fdf016ca04ba1662caa878cd2548
asecHTL <HumerS22022@sus.htl-grieskirchen.at>
2024-10-08 13:45:42 +0200

    Adding Freagment to Project

---
+++ app/src/main/java/at/htlgkr/dashboard/Dashboard.java
@@ -0,0 +1,37 @@
+package at.htlgkr.dashboard;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import at.htlgkr.dashboard.databinding.FragmentDashboardBinding;
+
+
+public class Dashboard extends Fragment {
+
+    private FragmentDashboardBinding binding;
+
+    public Dashboard() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        binding = FragmentDashboardBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+    }
+}
